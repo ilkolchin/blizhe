02c4d0abad101f3ae8f8ccf4fb657a5537c54eec
Ilya Kolchin <kolchinilya7@gmail.com>
2022-06-09 23:51:59 +0300

    add sanitize

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import "./styles/main.scss"
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/typography.css';
+import "./styles/main.scss";
 
 const lol = {
   apple: 1,
-  grape: 3
-}
+  grape: 3,
+};
 
 const foo = {
   bar: 1,
-}
+};
 
 console.log(lol, foo);
