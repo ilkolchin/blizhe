f10f42738f1860f912bb1dc69acd43ab524679e1
Ilya Kolchin <kolchinilya7@gmail.com>
2022-06-09 23:02:57 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles/main.scss"
+
+const lol = {
+  apple: 1,
+  grape: 3
+}
+
+const foo = {
+  bar: 1,
+}
+
+console.log(lol, foo);
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
+
+module.exports = {
+  mode: mode,
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "img/[hash][ext][query]",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.hbs/,
+        use: "handlebars-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset",
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+
+  devtool: "source-map",
+
+  devServer: {
+    static: "./dist",
+    hot: false,
+  },
+};
