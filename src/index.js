9425eba98aac6db61aa88336c7cadb2c5230980d
Ilya Kolchin <kolchinilya7@gmail.com>
2022-06-28 18:08:10 +0300

    add eventListener

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import 'sanitize.css';
-import 'sanitize.css/forms.css';
-import 'sanitize.css/typography.css';
+import "sanitize.css";
+import "sanitize.css/forms.css";
+import "sanitize.css/typography.css";
 import "./styles/index.scss";
 
-const lol = {
-  apple: 1,
-  grape: 3,
-};
-
-const foo = {
-  bar: 1,
-};
-
-console.log(lol, foo);
+import "./scripts/signUp__btn";
---
+++ src/scripts/signUp__btn.js
@@ -0,0 +1,11 @@
+const BtnActivation = () => {
+  const btn = document.getElementById('button');
+
+  const handleClick = () => {
+    console.log('click on Btn');
+  };
+
+  btn.addEventListener("click", handleClick);
+};
+
+BtnActivation();
