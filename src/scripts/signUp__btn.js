9b22308f73337e6d9f30d9ed790662f703c1037f
Ilya Kolchin <kolchinilya7@gmail.com>
2022-07-01 14:33:47 +0300

    add 4th section, change hover effect on menu bar

--- src/scripts/signUp__btn.js
+++ src/scripts/signUp__btn.js
@@ -1,8 +1,8 @@
 const BtnActivation = () => {
-  const btn = document.getElementById('button');
+  const btn = document.getElementById("button");
 
   const handleClick = () => {
-    console.log('click on Btn');
+    console.log("click on Btn");
   };
 
   btn.addEventListener("click", handleClick);
