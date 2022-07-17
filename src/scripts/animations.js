f0e5408c8a6f52e03387a3395c9377943e2306f0
Ilya Kolchin <kolchinilya7@gmail.com>
2022-07-17 15:08:01 +0300

    fixed: last section on mobile

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -11,29 +11,29 @@ AboutObserver.observe(document.querySelector(".about__image"));
 
 //Contacts
 
-// const ContactsObserver = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("slide-in-fwd-center-2");
-//     }
-//   });
-// });
-// ContactsObserver.observe(document.querySelector(".contacts__innerimages-1"));
+const ContactsObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("slide-in-fwd-center-2");
+    }
+  });
+});
+ContactsObserver.observe(document.querySelector(".contacts__images-img1"));
 
-// const ContactsObserver2 = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("slide-in-fwd-center");
-//     }
-//   });
-// });
-// ContactsObserver2.observe(document.querySelector(".contacts__innerimages-2"));
+const ContactsObserver2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("slide-in-fwd-center");
+    }
+  });
+});
+ContactsObserver2.observe(document.querySelector(".contacts__images-img2"));
 
-// const ContactsObserver3 = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("slide-in-fwd-center-3");
-//     }
-//   });
-// });
-// ContactsObserver3.observe(document.querySelector(".contacts__image"));
+const ContactsObserver3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("slide-in-fwd-center-3");
+    }
+  });
+});
+ContactsObserver3.observe(document.querySelector(".contacts__img"));
