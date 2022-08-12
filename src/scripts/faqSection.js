a5ae1f9b4d0b32054db1749f8f4786ba801fb04f
Ilya Kolchin <kolchinilya7@gmail.com>
2022-08-12 14:07:47 +0300

    added: about mob. version

--- src/scripts/faqSection.js
+++ src/scripts/faqSection.js
@@ -1,9 +1,5 @@
 const titles = document.querySelectorAll(".question__title");
 
-document.addEventListener("click", (event) => {
-  // console.log(event);
-});
-
 const ToggleText = (event) => {
   const block = event.composedPath()[2];
   block.classList.toggle("open");
